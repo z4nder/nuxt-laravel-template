c6f3f4955d8b15404079d49382889eadd5c27eca
z4nder <gusttavo212@gmail.com>
2023-04-14 11:17:32 -0300

    Add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    apiSecret: "my-app-secutiry-api-key",
+    public: {
+      apiBase: "localhost:8000/api",
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline text-red-500">
-            Hello world!
-        </h1>
-    </div>
+  <div>
+    <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
+  </div>
 </template>
-  
