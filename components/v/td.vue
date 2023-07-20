a5b20552886c4b9b4327b99965fab7e142b4e82b
z4nder <gusttavo212@gmail.com>
2023-07-20 18:49:40 -0300

    Create table component

---
+++ components/v/td.vue
@@ -0,0 +1,5 @@
+<template>
+  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+    <slot />
+  </td>
+</template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -20,114 +20,68 @@
           </router-link>
         </div>
       </div>
-      <div class="mt-8 flow-root">
-        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div
-            class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
-          >
-            <div
-              class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg"
-            >
-              <table class="min-w-full divide-y divide-gray-300">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                      ID
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Roles
-                    </th>
-                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span class="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
-                  <tr v-for="user in users" :key="user.id">
-                    <td
-                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
-                    >
-                      {{ user.id }}
-                    </td>
-                    <td
-                      class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
-                    >
-                      {{ user.name }}
-                    </td>
-                    <td
-                      class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
-                    >
-                      {{ user.email }}
-                    </td>
-                    <td
-                      class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
-                    >
-                      <span
-                        v-for="role in user.roles"
-                        :key="role"
-                        class="inline-flex items-center rounded-md bg-green-500/10 px-2 py-1 text-xs font-medium text-green-400 ring-1 ring-inset ring-green-500/20"
-                      >
-                        {{ role }}</span
-                      >
-                    </td>
-                    <td
-                      class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
-                    >
-                      <div class="flex gap-2">
-                        <a
-                          href="#"
-                          class="bg-secondary-500 hover:bg-secondary-700 p-2 rounded-md"
-                        >
-                          <PencilIcon
-                            class="h-5 w-5 text-white"
-                            aria-hidden="true"
-                        /></a>
-                        <a
-                          href="#"
-                          class="bg-red-500 hover:bg-red-700 p-2 rounded-md"
-                        >
-                          <TrashIcon
-                            class="h-5 w-5 text-white bg-text-500 hover:bg-text-700"
-                            aria-hidden="true"
-                        /></a>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      <v-table>
+        <template #thead>
+          <tr>
+            <v-th> ID </v-th>
+            <v-th> Name </v-th>
+            <v-th> Email </v-th>
+            <v-th> Roles </v-th>
+            <v-th>
+              <span class="sr-only">Edit</span>
+            </v-th>
+          </tr>
+        </template>
+        <template #tbody>
+          <tr v-for="user in users" :key="user.id">
+            <v-td>
+              {{ user.id }}
+            </v-td>
+            <v-td>
+              {{ user.name }}
+            </v-td>
+            <v-td>
+              {{ user.email }}
+            </v-td>
+            <v-td>
+              <span
+                v-for="role in user.roles"
+                :key="role"
+                class="inline-flex items-center rounded-md bg-green-500/10 px-2 py-1 text-xs font-medium text-green-400 ring-1 ring-inset ring-green-500/20"
+              >
+                {{ role }}</span
+              >
+            </v-td>
+            <v-td>
+              <div class="flex gap-2">
+                <a
+                  href="#"
+                  class="bg-secondary-500 hover:bg-secondary-700 p-2 rounded-md"
+                >
+                  <PencilIcon class="h-5 w-5 text-white" aria-hidden="true"
+                /></a>
+                <a href="#" class="bg-red-500 hover:bg-red-700 p-2 rounded-md">
+                  <TrashIcon
+                    class="h-5 w-5 text-white bg-text-500 hover:bg-text-700"
+                    aria-hidden="true"
+                /></a>
+              </div>
+            </v-td>
+          </tr>
+        </template>
+      </v-table>
     </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import { TrashIcon, PencilIcon } from "@heroicons/vue/24/outline";
-
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
 definePageMeta({ middleware: ["auth"] });
 const layout = "auth";
-
 const { fetchUsers, users } = useUser();
+
+const page = ref<Number>(1);
 
 onMounted(async () => {
   await fetchUsers();
